3e658a120ede4811ff4c92096ec3a30ef038af74
Pelegrinetti <pelegrinetti.dev@gmail.com>
2021-06-09 10:41:22 -0300

    Use cache and container

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"github.com/Pelegrinetti/trellenge-go/internal/config"
+	"github.com/Pelegrinetti/trellenge-go/pkg/cache"
+	"github.com/Pelegrinetti/trellenge-go/pkg/container"
 	"github.com/Pelegrinetti/trellenge-go/pkg/http"
 )
 
 func main() {
 	config := config.New()
-	s := http.New()
+	cacheClient := cache.New(config.CacheAddress, config.CachePassword)
+	ctn := container.New()
+
+	ctn.WithCache(cacheClient)
+
+	s := http.New(ctn)
 
 	s.Run(config.Port)
 }

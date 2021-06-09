f1dbdf064240460ff55a9f0e99ec55af2bade7c6
Pelegrinetti <pelegrinetti.dev@gmail.com>
2021-06-09 10:39:19 -0300

    Add cache config

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,13 +3,17 @@ package config
 import "github.com/spf13/viper"
 
 type Config struct {
-	AppName string `mapstructure:"APP_NAME"`
-	Port    int    `mapstructure:"PORT"`
+	AppName       string `mapstructure:"APP_NAME"`
+	Port          int    `mapstructure:"PORT"`
+	CacheAddress  string `mapstructure:"CACHE_ADDRESS"`
+	CachePassword string `mapstructure:"CACHE_PASSWORD"`
 }
 
 func New() *Config {
 	viper.SetDefault("APP_NAME", "trellenge-go")
 	viper.SetDefault("PORT", 3333)
+	viper.SetDefault("CACHE_ADDRESS", "localhost:6379")
+	viper.SetDefault("CACHE_PASSWORD", "")
 	viper.AutomaticEnv()
 
 	config := &Config{}
